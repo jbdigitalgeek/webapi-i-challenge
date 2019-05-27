1c0139b9290b6ac9e39f64072ef8ba325fbfad6c
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-05-27 14:53:59 -0700

    posting data with name/bio requirements

--- index.js
+++ index.js
@@ -2,23 +2,40 @@
 const express = require("express");
 const db = require("./data/db.js");
 
-
 const server = express();
 
+server.use(express.json());
+
 server.get("/api/users", (req, res) => {
-  db
-    .find()
+  db.find()
     .then(users => {
       res.status(200).json(users);
     })
     .catch(error => {
-      res.status(500).json({ error: "The users information could not be retrieved." });
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved." });
     });
 });
 
-server.post('/api/users', (req, res) => {
-    const user = req.body
-})
+server.post("/api/users", (req, res) => {
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    db.insert({ name, bio })
+      .then(response => {
+        res.status(201).json(response);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "There was an error while saving the user to the database"
+        });
+      });
+  }
+});
 
 server.listen(4000, () => {
   console.log("\n**server listening to port 4000**");
